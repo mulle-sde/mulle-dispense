98ddfc65b10335feb6514d6eebab1b0a43309e91
Nat! <nat@mulle-kybernetik.com>
2020-03-29 21:58:02 +0200

    slight code style improvement

--- src/mulle-dispense-copy.sh
+++ src/mulle-dispense-copy.sh
@@ -400,7 +400,7 @@ collect_and_dispense_product()
             IFS=$':'; set -f
             for i in ${sources}
             do
-               IFS="${DEFAULT_IFS}"; set +f
+               set +o noglob; IFS="${DEFAULT_IFS}"
 
                if [ -d "${i}" ]
                then
@@ -408,7 +408,7 @@ collect_and_dispense_product()
                   expanded="${RVAL}"
                fi
             done
-            IFS="${DEFAULT_IFS}"; set +f
+            set +o noglob; IFS="${DEFAULT_IFS}"
 
             sources="${expanded}"
          fi
