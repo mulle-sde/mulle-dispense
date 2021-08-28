9e716fb593cb7920143deba49adb7233876361a8
Nat! <nat@mulle-kybernetik.com>
2021-08-28 18:36:59 +0200

    * uses mulle-bashfunctions 4 now * can now run under zsh if bash is not available

--- src/mulle-dispense-copy.sh
+++ src/mulle-dispense-copy.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#! /bin/sh
 #
 #   Copyright (c) 2015-2017 Nat! - Mulle kybernetiK
 #   All rights reserved.
@@ -29,6 +29,68 @@
 #   POSSIBILITY OF SUCH DAMAGE.
 #
 MULLE_DISPENSE_COPY_SH="included"
+
+
+####
+#
+# Prelude to be placed at top of each script. Rerun this script either in
+# bash or zsh, if not already running in either (which can happen!)
+# Allows script to run on systems that either have bash (linux) or
+# zsh (macOS) only by default.
+
+if [ "$1" != --no-auto-shell ]
+then
+   if [ -z "${BASH_VERSION}" -a -z "${ZSH_VERSION}" ]
+   then
+      exe_shell="`command -v "bash" `"
+      exe_shell="${exe_shell:-`command -v "zsh" `}"
+
+      script="$0"
+
+      #
+      # Quote incoming arguments for shell expansion
+      #
+      args=""
+      for arg in "$@"
+      do
+         # True bourne sh doesn't know ${a//b/c} and <<<
+         case "${arg}" in
+            *\'*)
+               # Use cat instead of echo to avoid possible echo -n
+               # problems. Escape single quotes in string.
+               arg="`cat <<EOF | sed -e s/\'/\'\\\"\'\\\"\'/g
+${arg}
+EOF
+`"
+            ;;
+         esac
+         if [ -z "${args}" ]
+         then
+            args="'${arg}'"
+         else
+            args="${args} '${arg}'"
+         fi
+      done
+
+      #
+      # bash/zsh will use arg after -c <arg> as $0, convenient!
+      #
+
+      exec "${exe_shell:-bash}" -c ". ${script} --no-auto-shell ${args}" "${script}"
+   fi
+   if [ ! -z "${BASH_VERSION}" ]
+   then
+      set +o posix
+   fi
+else
+   shift    # get rid of --no-auto-shell
+fi
+
+
+#
+# Main script to follow, runs now either in zsh or bash
+#
+####
 
 
 dispense_usage()
@@ -397,10 +459,10 @@ collect_and_dispense_product()
          then
             local expanded
 
-            IFS=$':'; set -f
+            IFS=$':'; shell_disable_glob
             for i in ${sources}
             do
-               set +f; IFS="${DEFAULT_IFS}"
+               shell_enable_glob; IFS="${DEFAULT_IFS}"
 
                if [ -d "${i}" ]
                then
@@ -408,7 +470,7 @@ collect_and_dispense_product()
                   expanded="${RVAL}"
                fi
             done
-            set +f; IFS="${DEFAULT_IFS}"
+            shell_enable_glob; IFS="${DEFAULT_IFS}"
 
             sources="${expanded}"
          fi
